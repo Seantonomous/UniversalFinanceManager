9a3c3aefb47e28c1c80928044b7c342a4b721e85
Seantonomous <seanmhansen7@gmail.com>
2017-09-30 15:04:59 -0700

    Added new theme to remove annoying titlebar, Added event listeners to login page buttons, remove unecessary xml namespace

--- app/src/main/java/ufm/universalfinancemanager/Login_Activity.java
+++ app/src/main/java/ufm/universalfinancemanager/Login_Activity.java
@@ -2,12 +2,60 @@ package ufm.universalfinancemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login_Activity extends AppCompatActivity {
+    Button login_button, signup_button;
+    EditText email_field, password_field;
+
+    String user_email, user_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+
+        login_button = (Button)findViewById(R.id.login_button);
+        signup_button = (Button)findViewById(R.id.signup_button);
+
+        email_field = (EditText)findViewById(R.id.email_field);
+        password_field = (EditText)findViewById(R.id.pass_field);
+
+        //Login button event listener
+        login_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        user_email = email_field.getText().toString();
+                        user_password = password_field.getText().toString();
+
+                        //Authentication will go here, for now just redirect
+                        //TO-DISCUSS: SQL Injection prevention
+
+                        login();
+                    }
+                }
+        );
+
+        signup_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        //Perform first time user setup
+                    }
+                }
+        );
+    }
+
+    void login() {
+        //Switch to main application activity
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
