acd0134d875cbd63b8ce39e47e4266279a153dbe
Seantonomous <seanmhansen7@gmail.com>
2017-10-02 22:17:28 -0700

    Created main activity (w/ Nav Drawer), set login to switch to main activity, updated manifest

---
+++ app/src/main/java/ufm/universalfinancemanager/Main_Activity.java
@@ -0,0 +1,17 @@
+package ufm.universalfinancemanager;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+
+    }
+}
