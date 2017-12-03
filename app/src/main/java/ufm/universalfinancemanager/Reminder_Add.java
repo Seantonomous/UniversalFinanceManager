9858163399531d3dac918752592f7588b91ba679
reabz <waheed.areeba.97@gmail.com>
2017-12-02 16:50:56 -0800

    changed the theme, added date picker in reminder, changed logo

--- app/src/main/java/ufm/universalfinancemanager/Reminder_Add.java
+++ app/src/main/java/ufm/universalfinancemanager/Reminder_Add.java
@@ -1,20 +1,81 @@
 package ufm.universalfinancemanager;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 /**
  * Created by Areeba on 11/2/2017.
  */
 
 public class Reminder_Add extends Activity {
+
+    private EditText edit_date;
+    private TextView date_textView;
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private int year, month, day;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_reminder);
+
+
+        edit_date = (EditText)findViewById(R.id.date);
+        date_textView = (TextView)findViewById(R.id.dateTextView);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year,month+1,day);
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "Select Date",
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        edit_date.setText(new StringBuilder().append(day).append(" / ")
+                .append(month).append(" / ").append(year));
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
